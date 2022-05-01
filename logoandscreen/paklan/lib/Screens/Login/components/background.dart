b2550b07b6966747a0ded3f1128b65971075b1b2
Pariss Ananda <pjff18@gmail.com>
2022-05-01 04:38:43 -0500

    pak

---
+++ logoandscreen/paklan/lib/Screens/Login/components/background.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Background extends StatelessWidget {
+  final Widget child;
+  const Background({
+    Key? key=null,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: size.height,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Positioned(
+            top: -76,
+            left: -40,
+            child: Image.asset(
+              "assets/images/explot.png",
+              width: size.width * 0.75,
+            ),
+          ),
+          Positioned(
+            bottom: -40,
+            right: -60,
+            child: Image.asset(
+              "assets/images/algodon.png",
+              width: size.width * 0.6,
+            ),
+          ),
+          child,
+        ],
+      ),
+    );
+  }
+}
--- logoandscreen/paklan/lib/main.dart
+++ logoandscreen/paklan/lib/main.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:paklan/Screens/Welcome/welcome_screen.dart';
+import 'package:paklan/constants.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Auth',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
       ),
+      home: WelcomeScreen(),
     );
   }
 }
